99d4240fe697cb735730daaa9a484b2e828883f2
Mal Marconi <106759552+mmarconi93@users.noreply.github.com>
2024-05-19 09:17:29 -0500

    edited callbacks

--- pkg/callbacks/callback.go
+++ pkg/callbacks/callback.go
@@ -1,8 +1,29 @@
 package callbacks
 
-import "github.com/sirupsen/logrus"
+import (
+    "encoding/json"
+    "fmt"
+    "io"
+    "log"
+    "os"
 
-// PrintErrorMessage prints an error message using logrus
+    "github.com/Azure/azure-sdk-for-go/sdk/azcore"
+)
+
+// PrintErrorMessage prints detailed error messages for Azure SDK errors
 func PrintErrorMessage(err error) {
-    logrus.WithError(err).Error("Operation failed")
+    var respError struct {
+        Code    string `json:"code"`
+        Details string `json:"details"`
+        Message string `json:"message"`
+        Subcode string `json:"subcode"`
+    }
+    if httpErr, ok := err.(*azcore.ResponseError); ok {
+        body, _ := io.ReadAll(httpErr.RawResponse.Body)
+        json.Unmarshal(body, &respError)
+        fmt.Printf("Error: %s\n", respError.Message)
+        os.Exit(1)
+    } else {
+        log.Fatal(err)
+    }
 }
